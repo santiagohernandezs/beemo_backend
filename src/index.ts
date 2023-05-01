a58422e34e1ce93432caed052b09280e64ce696c
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-05-01 12:48:06 -0400

    chore: setting up apollo server and prisma models

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('Hello World from index.ts')
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import gql from 'graphql-tag'
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Query {
+    hello: String!
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+    users: () => []
+  },
+  Mutation: {
+    createUser: () => ({
+      id: '1',
+      name: 'John',
+      email: 'j@prisma.io'
+    })
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+})
+
+console.log(`Server ready at ${url}🚀`)
