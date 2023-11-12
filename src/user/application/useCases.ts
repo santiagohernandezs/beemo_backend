dd3ee9bf5abfceb1feecd9a3ba4584e480400741
Santiago Hernandez <santiago.hernandez@creaapps.com.do>
2023-11-12 09:20:31 -0400

    :memo: Add user documentation and update project settings
    add docs for controllers and use cases and set project settings

--- src/user/application/useCases.ts
+++ src/user/application/useCases.ts
@@ -1,29 +1,47 @@
+import type { User } from '@prisma/client'
 import { raise } from '@shared/helpers/errors'
 import { deletedUser, newUser, user, users } from '@user/domain/controllers'
-import type { User, UserDTO } from '@user/types/core/types'
+import type { UserDTO } from '@user/types/core/types'
 import jwt from 'jsonwebtoken'
 
-const createUser = async (args: UserDTO) =>
+/**
+ * Create a new user
+ *
+ * @param args - The user data as a UserDTO
+ * @returns The user created
+ */
+
+const createUser = async (args: UserDTO): Promise<User> =>
   newUser({
     data: {
       ...args
     }
   })
 
-const getUsers = async () =>
+/**
+ * Get all the users
+ *
+ * @returns The all users
+ */
+
+const getUsers = async (): Promise<User[]> =>
   users({
     include: {
       tickets: true
     }
   })
 
-const loginUser = async (args: Pick<UserDTO, 'email' | 'password'>) => {
+/**
+ * Login user
+ *
+ * @param args - The user data as a email and password
+ * @returns The user token
+ */
+
+const loginUser = async (args: Pick<UserDTO, 'email' | 'password'>): Promise<{ token: string }> => {
   const { email, password } = args
 
-  const currentUser = (await user({ where: { email } })) as Pick<
-    User,
-    'id' | 'password' | 'email'
-  >
+  const currentUser = (await user({ where: { email } })) as Pick<User, 'id' | 'password' | 'email'>
 
   if (!currentUser) raise('User', 'User not found')
   if (currentUser.password !== password) raise('Auth', 'Password not match')
@@ -31,7 +49,7 @@ const loginUser = async (args: Pick<UserDTO, 'email' | 'password'>) => {
   const userToken = jwt.sign(
     {
       email: currentUser.email,
-      id: currentUser?.id
+      id: currentUser.id
     },
     process.env.SECRET_KEY || 'secret',
     {
@@ -45,9 +63,22 @@ const loginUser = async (args: Pick<UserDTO, 'email' | 'password'>) => {
   }
 }
 
-const getUserById = async (id: string) => await user({ where: { id } })
+/**
+ * Get user by id
+ *
+ * @param args - Id of the user
+ * @returns The user with the id passed
+ */
 
-const removeUser = async (id: string) => await deletedUser({ where: { id } })
+const getUserById = async (id: string): Promise<User | null> => await user({ where: { id } })
+
+/**
+ * Remove user
+ *
+ * @param args - Id of the user
+ * @returns The user removed
+ */
+
+const removeUser = async (id: string): Promise<User> => await deletedUser({ where: { id } })
 
 export { createUser, getUserById, getUsers, loginUser, removeUser }
-
--- src/user/domain/controllers.ts
+++ src/user/domain/controllers.ts
@@ -1,32 +1,78 @@
 import prisma from '@db/connection'
-import { type Prisma } from '@prisma/client'
+import type { Prisma, User } from '@prisma/client'
 import { raise } from '@shared/helpers/errors'
 
-const newUser = async (args: Prisma.UserCreateArgs) =>
+/**
+ * Create a new user
+ *
+ * @param args - The user data as UserCreateArgs from Prisma
+ * @returns The user created as User from Prisma
+ *
+ * @example newUser({ data: { ...UserData } }) // Create a new user
+ */
+const newUser = async (args: Prisma.UserCreateArgs): Promise<User> =>
   await prisma.user
     .create(args)
     .catch(err => raise('User', 'Cannot create user', err))
     .finally(async () => await prisma.$disconnect())
 
-const users = async (args: Prisma.UserFindManyArgs) =>
+/**
+ * Find many users
+ *
+ * @param args - The user data as UserFindManyArgs from Prisma
+ * @returns A list of users as User[] from Prisma
+ *
+ * @example users({ where: { name: 'John' } }) // Find all users named John
+ * @example users({ where: { role: 'ADMIN' } }) // Find all users with role ADMIN
+ *
+ */
+const users = async (args: Prisma.UserFindManyArgs): Promise<User[]> =>
   await prisma.user
     .findMany(args)
     .catch(err => raise('User', 'Cannot find users', err))
     .finally(async () => await prisma.$disconnect())
 
-const user = async (args: Prisma.UserFindUniqueArgs) =>
+/**
+ * Find one user
+ *
+ * @param args - The user data as UserFindUniqueArgs from Prisma
+ * @returns The user found as User from Prisma
+ *
+ * @example user({ where: { id: 1 } }) // Find user with id 1
+ * @example user({ where: { email: {startsWith: 'john'} } }) // Find user with email starting with john
+ */
+const user = async (args: Prisma.UserFindUniqueArgs): Promise<User | null> =>
   await prisma.user
     .findUnique(args)
     .catch(err => raise('User', 'Cannot find user', err))
     .finally(async () => await prisma.$disconnect())
 
-const deletedUser = async (args: Prisma.UserDeleteArgs) =>
+/**
+ * Delete one user
+ *
+ * @param args - The user data as UserDeleteArgs from Prisma
+ * @returns The user deleted as User from Prisma
+ *
+ * @example deletedUser({ where: { id: 1 } }) // Delete user with id 1
+ * @example deletedUser({ where: { email: {startsWith: 'john'} } }) // Delete user with email starting with john
+ */
+const deletedUser = async (args: Prisma.UserDeleteArgs): Promise<User> =>
   await prisma.user
     .delete(args)
     .catch(err => raise('User', 'Cannot delete user', err))
     .finally(async () => await prisma.$disconnect())
 
-const updatedUser = async (args: Prisma.UserUpdateArgs) =>
+/**
+ * Update one user
+ *
+ * @param args - The user data as UserUpdateArgs from Prisma
+ * @returns The user updated as User from Prisma
+ *
+ * @example updatedUser({ where: { id: 1 }, data: { name: 'John' } }) // Update user with id 1
+ * @example updatedUser({ where: { email: {startsWith: 'john'} }, data: { name: 'John' } }) // Update user with email starting with john
+ *
+ */
+const updatedUser = async (args: Prisma.UserUpdateArgs): Promise<User> =>
   await prisma.user
     .update(args)
     .catch(err => raise('User', 'Cannot update user', err))
